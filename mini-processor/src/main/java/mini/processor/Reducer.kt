6c06dad641c8731ab74956b7d53e61330719cd92
Francisco García Sierra <francisco.garciasierra@bq.com>
2018-05-12 22:24:32 +0200

    More code generation, WIP

--- mini-processor/src/main/java/mini/processor/Reducer.kt
+++ mini-processor/src/main/java/mini/processor/Reducer.kt
@@ -1,9 +1,13 @@
 package mini.processor
 
-import mini.Action
 import javax.lang.model.element.ExecutableElement
 
 class ReducerFunc(executableElement: ExecutableElement) {
-    val action: Action = executableElement.parameters[0]!!.constantValue as Action
+    val action: ActionModel = ActionModel(executableElement.parameters[0].asType().asElement())
+    val funcName : String = executableElement.simpleName.toString()
     val parentClass = executableElement.enclosingElement
+
+    fun toWhenClause() : String{
+        return "is ${action.actionName} -> {"
+    }
 }
