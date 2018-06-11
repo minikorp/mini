a54971f49d0731446719bae5e632a73c3b71ee90
Pablo Orgaz <pablo.orgaz@bq.com>
2018-06-11 16:31:09 +0200

    Add merge state function

--- mini-common/src/main/java/mini/RxUtils.kt
+++ mini-common/src/main/java/mini/RxUtils.kt
@@ -36,3 +36,33 @@ fun combineStores(vararg stores: Store<*>): Flowable<Store<*>> {
             acc.mergeWith(storeFlowable)
         }
 }
+
+/**
+ * Builder function for [StateMerger].
+ */
+inline fun <R> mergeStates(crossinline builder: StateMerger<R>.() -> Unit): Flowable<List<R>> {
+    return StateMerger<R>().apply { builder() }.merge()
+}
+
+/**
+ * Combine multiple store state flowables into a list of type R. If multiple stores
+ * change at the same time only one value is emitted.
+ */
+class StateMerger<R> {
+    val storeAndMappers = ArrayList<Pair<Store<*>, () -> R>>()
+
+    inline fun <T : R, S : Store<U>, U : Any> merge(store: S, crossinline mapper: (U.() -> T)) {
+        storeAndMappers.add(store to { store.state.mapper() })
+    }
+
+    fun merge(): Flowable<List<R>> {
+        return storeAndMappers
+            .map { (store, fn) -> store.flowable().select { fn() } }
+            .reduce { acc, storeFlowable ->
+                acc.mergeWith(storeFlowable)
+            }
+            .map {
+                storeAndMappers.map { (_, fn) -> fn() }.toList()
+            }
+    }
+}
