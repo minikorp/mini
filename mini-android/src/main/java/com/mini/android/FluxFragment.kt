17302add02fddd49fb3c9d290fe544c56377787a
Pablo Orgaz <pablo.orgaz@movo.me>
2020-01-21 12:51:50 +0100

    Add utility methods for FluxFragment and FluxActivity to work with coroutines

--- mini-android/src/main/java/com/mini/android/FluxFragment.kt
+++ mini-android/src/main/java/com/mini/android/FluxFragment.kt
@@ -1,8 +1,12 @@
 package com.mini.android
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.launch
 import mini.CloseableTracker
 import mini.DefaultCloseableTracker
 import mini.DispatcherContainer
@@ -12,9 +16,41 @@ abstract class FluxFragment : Fragment(),
                               CloseableTracker by DefaultCloseableTracker(),
                               CoroutineScope,
                               DispatcherContainer {
+
     override val coroutineContext: CoroutineContext
         get() = lifecycleScope.coroutineContext
 
     override val defaultDispatchScope: CoroutineScope
         get() = lifecycleScope
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        lifecycleScope.launch { whenCreated(savedInstanceState) }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        lifecycleScope.launch { whenResumed() }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        lifecycleScope.launch { whenPaused() }
+    }
+
+    override fun onDestroy() {
+        lifecycleScope.launch { whenDestroyed() }
+        clearCloseables()
+        super.onDestroy()
+    }
+
+    fun <T> Flow<T>.launchOnUi() {
+        launchIn(lifecycleScope)
+    }
+
+    protected open suspend fun whenCreated(savedInstanceState: Bundle?) = Unit
+    protected open suspend fun whenResumed() = Unit
+    protected open suspend fun whenPaused() = Unit
+    protected open suspend fun whenDestroyed() = Unit
 }
